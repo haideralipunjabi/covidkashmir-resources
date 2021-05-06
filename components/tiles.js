0d30ca4257828d1ba40c6043e54c5ab2fb71a587
Haider Ali Punjabi <haiderali@cyberservices.com>
2021-05-06 22:35:29 +0530

    Added CovidReliefJk & LinkTree

--- components/tiles.js
+++ components/tiles.js
@@ -10,7 +10,9 @@ export default function Tiles(){
         {"title": "Helplines","icon":["fas","phone"],"link":"helpline"},
         {"title": "Doctors","icon":["fas","user-md"],"link":"doctor"},
         {"title": "Meals","icon":["fas","utensils"],"link":"meal"},
-        {"title": "Tweet SOS", "icon":["fab","twitter"],"link":"https://twitter.com/intent/tweet?text=%0A%0A%23SOSJK%20%23SOSKashmir%0A%40Altxk%20%40livenletdiepls%20%40thenotoriousimi%20%40COVIDKashmir%20%40Azzharmalik%20%40jkcovid2021%20%40rj_vijdan%20%40sro_kashmir%20%40Athrout_Kashmir%20"}
+        {"title": "Tweet SOS", "icon":["fab","twitter"],"link":"https://twitter.com/intent/tweet?text=%0A%0A%23SOSJK%20%23SOSKashmir%0A%40Altxk%20%40livenletdiepls%20%40thenotoriousimi%20%40COVIDKashmir%20%40Azzharmalik%20%40jkcovid2021%20%40rj_vijdan%20%40sro_kashmir%20%40Athrout_Kashmir%20"},
+        {"title": "CovidReliefJK", "icon":["fas","user-shield"], "link":"https://covidrelief.jk.gov.in/"},
+        {"title": "Other Links", "icon":["fas","external-link-alt"], "link":"https://linktr.ee/KashmirCovidResources"}
     ]
 
     return (
